24130465fdf4dfc33e8618065f43ef1ca11b5353
santanu kumar apadhy <santunupadhy2@gmail.com>
2024-07-11 16:15:45 +0530

    first commit

---
+++ src/media/rajasthan/RajasthanImages.jsx
@@ -0,0 +1,31 @@
+import port1 from './port1.jpg';
+import port2 from './port2.jpg';
+import fam1 from './fam1.jpg';
+import fam2 from './fam2.jpg';
+import cult1 from './cult1.jpg';
+import cult2 from './cult2.jpg';
+import food1 from './food1.jpg';
+import food2 from './food2.jpg';
+import pack1 from './pack1.jpg';
+import pack2 from './pack2.jpg';
+import pack3 from './pack3.jpg';
+import pack4 from './pack4.jpg';
+import only1 from './only1.jpg';
+import only2 from './only2.jpg';
+
+export const RajasthanImages = {
+    d1: port1,
+    d2: port2,
+    db1: fam1,
+    db2: fam2,
+    dc1: cult1,
+    dc2: cult2,
+    dh1: food1,
+    dh2: food2,
+    dp1: pack1,
+    dp2: pack2,
+    dp3: pack3,
+    dp4: pack4,
+    do1: only1,
+    do2: only2,
+};
